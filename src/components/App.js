d4b722bbf46422c2be34624ce8db5a559210f09e
ritikj0536 <jharitik64@gmail.com>
2026-01-18 11:41:56 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,18 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement } from "./action";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
 
 const App = () => {
   const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
+  const count = useSelector((state) => state.counter.value);
 
   return (
     <div>
       <h1>{count}</h1>
 
       <div>
-        <span>Counter</span>
-
-        <button
-          type="button"
-          onClick={() => dispatch(increment())}
-        >
-          increment
-        </button>
-
-        <button
-          type="button"
-          onClick={() => dispatch(decrement())}
-        >
-          decrement
-        </button>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
       </div>
     </div>
   );
--- src/components/store.js
+++ src/components/store.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
-import counterReducer from "./reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice";
 
-const store = createStore(counterReducer);
-
-export default store;
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
